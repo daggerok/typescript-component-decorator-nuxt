33fb07ed03bb5571b3aaba59ff037e5668f8ccef
Maksim Kostromin <daggerok@gmail.com>
2019-12-20 02:40:07 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -12,13 +12,11 @@
 
 <!-- THIS lang="ts" IS REALLY IMPORTANT! -->
 <script lang="ts">
-  import { Component, Ref, Vue } from 'vue-property-decorator';
+  import { Component, Vue } from 'vue-property-decorator';
   import UserView from '~/components/UserView.vue';
 
   @Component({
     components: { UserView }
   })
-  export default class IndexPage extends Vue {
-    @Ref() readonly userView!: UserView;
-  }
+  export default class IndexPage extends Vue { }
 </script>
